274d126b018b1c82d18d2ccf958eb0b469b3aa67
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-09-11 20:02:59 +0300

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,9 @@ inputEl.addEventListener("blur", onInputValidation);
 function onInputValidation(event) {
     if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length)) {
         inputEl.classList.add("valid");
+        inputEl.classList.remove("invalid");
     } else {
         inputEl.classList.add("invalid");
+        inputEl.classList.remove("valid");
     }
 }
